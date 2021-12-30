0bdbeb9fffa7f6399af999703319e24cf996f514
saurabh <saurabhkedar@gmail.com>
2021-12-31 02:08:39 +0530

    design structure created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import Data from './MOCK_DATA.json'
+import './App.css'
+import { useState } from 'react'
 function App() {
+  const [searchData, setSearchData] = useState('')
   return (
     <>
-      <div className='App'>
-        <input type='text' placeholder='Search data...' />
+      <div className='searchbar'>
+        <div className='App'>
+          <input
+            className='search'
+            type='text'
+            placeholder='Search data...'
+            onChange={(e) => {
+              setSearchData(e.target.value)
+            }}
+          />
+        </div>
+        <div className='database'>
+          {Data.filter((val) => {
+            if (searchData == '') {
+              return val
+            } else if (
+              val.BookName.toLowerCase().includes(searchData.toLowerCase())
+            ) {
+              return val
+            }
+          }).map((val, key) => {
+            return (
+              <div className='library' key={key}>
+                <p> {val.BookName}</p>
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   )
